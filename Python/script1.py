d2ab584a7d93b87e0f0bf886d6e9550441d84b43
Tripura kant <tripurakant@gmail.com>
2022-03-28 01:34:36 +0530

    new

--- Python/script1.py
+++ Python/script1.py
@@ -1,68 +1,68 @@
-# # My First Python Programming
+# # # My First Python Programming
 
 
 
 
-# print("Hello world")
+# # print("Hello world")
 
-# #Print Hello worl and cursor at last line.
-# print('Hello world', end='')
+# # #Print Hello worl and cursor at last line.
+# # print('Hello world', end='')
 
-# #Greeting.py
-# #The following program asks the user their names and greets them.
-# #print("hello " + input("What is your name ? "))
+# # #Greeting.py
+# # #The following program asks the user their names and greets them.
+# # #print("hello " + input("What is your name ? "))
 
-# print("Hello Coursera")
+# # print("Hello Coursera")
 
-# print("Welcome to coursera " + input("What is your name? "))
+# # print("Welcome to coursera " + input("What is your name? "))
 
-# #Week2 
-# #Data Types
+# # #Week2 
+# # #Data Types
 
-# ## Agenda of Week 2
+# # ## Agenda of Week 2
 
-# #Develop program that utlilize integers
-# #Develop programs that develop decimal numbers
-# #boolena
-# #simple string
-# #out of existence
-
-
-
-#Primitive data type
-
-#String data type
-#Integer Whole numbers
-#Float Numbers and Decimal places
-
-##How to Declare and Initialize a Variable
-# >> com
-# We donot declare variables
-# They are created as first assignment
-
-# age=53
+# # #Develop program that utlilize integers
+# # #Develop programs that develop decimal numbers
+# # #boolena
+# # #simple string
+# # #out of existence
 
 
-## Arithmetic Operation
 
-# Addition     x + Y
-# Substraction x - y
-# Multiplication x * y
-# Division x / Y
-# Modulus x % y
-# Exponentiation x ** y
-# Floor Division x // y
+# #Primitive data type
 
-# Integer Arithmetic
+# #String data type
+# #Integer Whole numbers
+# #Float Numbers and Decimal places
+
+# ##How to Declare and Initialize a Variable
+# # >> com
+# # We donot declare variables
+# # They are created as first assignment
+
+# # age=53
 
 
-x = 10
-y = 20
-result = x + y
+# ## Arithmetic Operation
 
-print(result)
+# # Addition     x + Y
+# # Substraction x - y
+# # Multiplication x * y
+# # Division x / Y
+# # Modulus x % y
+# # Exponentiation x ** y
+# # Floor Division x // y
 
-To be continued.
-https://www.coursera.org/learn/python-scripting-intro/supplement/yQIRT/string-data-type-reading
+# # Integer Arithmetic
+
+
+# x = 10
+# y = 20
+# result = x + y
+
+# print(result)
+
+# To be continued.
+# https://www.coursera.org/learn/python-scripting-intro/supplement/yQIRT/string-data-type-reading
 
 
