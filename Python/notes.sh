490384b2ebf399054c514555e7f06fbefeee4e1f
Tripura kant <tripurakant@gmail.com>
2022-03-27 09:11:08 +0530

    new

---
+++ Python/notes.sh
@@ -0,0 +1,2 @@
+# Python is a intrepreted language
+
---
+++ Python/python-scripting-devops.sh
@@ -0,0 +1,9 @@
+## Coursera course
+
+Benefit of Python
+#No need to declare variables
+#Dyanmic vs static Typing
+
+#Scripting vs Programming
+
+
