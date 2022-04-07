335cec92c6e95e2c3352ee56d01ce6373a70e2b9
Tripura kant <tripurakant@gmail.com>
2022-04-07 22:27:27 +0530

    new

---
+++ shell-scripting/Hackerrank/programs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Hacker 
+read x
+read y
+
+if [ $x -lt $y ]; then
+    echo "X is less than Y"
+elif [ $x -gt $y ]; then
+    echo "X is greater than Y"
+else
+    echo "X is equal to Y"
+fi
+
+
+#Hacker rank
+
+read X
+read Y
+
+echo $(( X + Y ))
+echo $(( X - Y ))
+echo $(( X * Y ))
+echo $(( X / Y ))
+
+
