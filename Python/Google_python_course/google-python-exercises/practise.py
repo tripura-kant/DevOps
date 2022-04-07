54e626c53f05fd2259a4d5d360f421237a3b320a
Tripura kant <tripurakant@gmail.com>
2022-04-07 13:05:12 +0530

    new

--- Python/Google_python_course/google-python-exercises/practise.py
+++ Python/Google_python_course/google-python-exercises/practise.py
@@ -1,7 +1,17 @@
-##
+# ##
 
-def Hello(name)
-if name == 'Alice' or name == 'Nick' :
-    name = name + '???'
-    else:
-        print 'Else'
+# def Hello(name)
+# if name == 'Alice' or name == 'Nick' :
+#     name = name + '???'
+#     else :
+#         print 'Else'
+#         name = name + '!!!'
+#         print 'Hello' , name
+
+###
+a = "Tripura"
+a + "Kant"
+
+
+#Formatiing in python
+'Hi %s I have %d donuts' % ('Alice', 42)
